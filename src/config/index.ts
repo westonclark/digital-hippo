97c210dee73934c56a5a5547b3789cab6b7ed13f
Weston Clark <westoclark@gmail.com>
2023-12-14 14:15:41 -0800

    add nav items and interactivity

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,19 @@ export const PRODUCT_CATEGORIES = [
   {
     label: 'UI Kits',
     value: 'ui_kits' as const,
-    featured: [{ name: 'Editor Picks', href: '#', imageSrc: '/nav/ui-kits/mixed.jpeg' }],
+    featured: [
+      { name: 'Editor Picks', href: '#', imageSrc: '/nav/ui-kits/mixed.jpeg' },
+      { name: 'New Arrivals', href: '#', imageSrc: '/nav/ui-kits/blue.jpeg' },
+      { name: 'Best Sellers', href: '#', imageSrc: '/nav/ui-kits/purple.jpeg' },
+    ],
+  },
+  {
+    label: 'Icons',
+    value: 'icons' as const,
+    featured: [
+      { name: 'Favorite Icon Picks', href: '#', imageSrc: '/nav/icons/picks.jpeg' },
+      { name: 'New Arrivals', href: '#', imageSrc: '/nav/icons/new.jpeg' },
+      { name: 'Best Sellers', href: '#', imageSrc: '/nav/icons/bestsellers.jpeg' },
+    ],
   },
 ];
