467e59c62f5cc36c7d7f6e30c91ce8cb519e1244
Weston Clark <westoclark@gmail.com>
2023-12-14 13:54:41 -0800

    init navbar and logo

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: 'UI Kits',
+    value: 'ui_kits' as const,
+    featured: [{ name: 'Editor Picks', href: '#', imageSrc: '/nav/ui-kits/mixed.jpeg' }],
+  },
+];
