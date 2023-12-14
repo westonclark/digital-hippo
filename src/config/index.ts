b44cdee758dda7f30bf7c6efa96efd3040c9ab05
Weston Clark <westoclark@gmail.com>
2023-12-14 15:02:45 -0800

    update navbar image src's

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,18 +3,18 @@ export const PRODUCT_CATEGORIES = [
     label: 'UI Kits',
     value: 'ui_kits' as const,
     featured: [
-      { name: 'Editor Picks', href: '#', imageSrc: '/nav/ui-kits/mixed.jpeg' },
-      { name: 'New Arrivals', href: '#', imageSrc: '/nav/ui-kits/blue.jpeg' },
-      { name: 'Best Selling Kits', href: '#', imageSrc: '/nav/ui-kits/purple.jpeg' },
+      { name: 'Editor Picks', href: '#', imageSrc: '/nav/ui-kits/mixed.jpg' },
+      { name: 'New Arrivals', href: '#', imageSrc: '/nav/ui-kits/blue.jpg' },
+      { name: 'Best Selling Kits', href: '#', imageSrc: '/nav/ui-kits/purple.jpg' },
     ],
   },
   {
     label: 'Icons',
     value: 'icons' as const,
     featured: [
-      { name: 'Favorite Icon Picks', href: '#', imageSrc: '/nav/icons/picks.jpeg' },
-      { name: 'New Arrivals', href: '#', imageSrc: '/nav/icons/new.jpeg' },
-      { name: 'Best Selling Icons', href: '#', imageSrc: '/nav/icons/bestsellers.jpeg' },
+      { name: 'Favorite Icon Picks', href: '#', imageSrc: '/nav/icons/picks.jpg' },
+      { name: 'New Arrivals', href: '#', imageSrc: '/nav/icons/new.jpg' },
+      { name: 'Best Selling Icons', href: '#', imageSrc: '/nav/icons/bestsellers.jpg' },
     ],
   },
 ];
