bd4698d694d5ea15c27ac0ed254425ac561dbf0a
Weston Clark <westoclark@gmail.com>
2023-12-16 12:52:02 -0800

    copmlete empty cart logic

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Cart = () => {
-  const itemCount = 1;
+  const itemCount = 0;
   const fee = 1;
 
   return (
@@ -60,6 +60,12 @@ const Cart = () => {
             <div className="relative mb-4 h-60 w-60 text-muted-foreground">
               <Image src="/hippo-empty-cart.png" alt="empty shopping cart hippo" fill />
             </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link href="/products" className={buttonVariants({ variant: 'link', size: 'sm', className: 'text-sm text-muted-foreground' })}>
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
           </div>
         )}
       </SheetContent>
