364fd853fc1030ef647c1dd8c095921865b50e9f
Weston Clark <westoclark@gmail.com>
2023-12-22 13:56:28 -0800

    diplay errors for sign-up inputs

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -5,13 +5,13 @@ import { Button, buttonVariants } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { cn } from '@/lib/utils';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { trpc } from '@/trpc/client';
 
-import { TAuthCredentialsValidator, AuthCredentialsValidator } from '@/lib/validators/account-credentials-validator';
+import { AuthCredentialsValidator, TAuthCredentialsValidator } from '@/lib/validators/account-credentials-validator';
+import { trpc } from '@/trpc/client';
 import { toast } from 'sonner';
 import { ZodError } from 'zod';
 import { useRouter } from 'next/navigation';
@@ -21,26 +21,31 @@ const Page = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<TAuthCredentialsValidator>({ resolver: zodResolver(AuthCredentialsValidator) });
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
 
   const router = useRouter();
 
   const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
-    onError: (error) => {
-      if (error.data?.code === 'CONFLICT') {
-        toast.error('This email is already in use. Please Sign in instead');
+    onError: (err) => {
+      if (err.data?.code === 'CONFLICT') {
+        toast.error('This email is already in use. Sign in instead?');
+
         return;
       }
 
-      if (error instanceof ZodError) {
-        toast.error(error.issues[0].message);
+      if (err instanceof ZodError) {
+        toast.error(err.issues[0].message);
+
         return;
       }
-      toast.error('Something went wrong, please try again');
+
+      toast.error('Something went wrong. Please try again.');
     },
     onSuccess: ({ sentToEmail }) => {
-      toast.success(`Verification email sent to ${sentToEmail}`);
-      router.push(`/verify-email?to=${sentToEmail}`);
+      toast.success(`Verification email sent to ${sentToEmail}.`);
+      router.push('/verify-email?to=' + sentToEmail);
     },
   });
 
@@ -54,24 +59,47 @@ const Page = () => {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
-            <h1 className="font-2xl font-bold">Create an account</h1>
-            <Link href="/sign-in" className={buttonVariants({ variant: 'link', className: 'gap-1.5' })}>
+            <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
+
+            <Link
+              className={buttonVariants({
+                variant: 'link',
+                className: 'gap-1.5',
+              })}
+              href="/sign-in">
               Already have an account? Sign-in
               <ArrowRight className="h-4 w-4" />
             </Link>
           </div>
+
           <div className="grid gap-6">
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="grid gap-2">
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="email">Email</Label>
-                  <Input className={cn({ 'focus-visible:ring-red-500': errors.email })} placeholder="you@example.com" {...register('email')} />
+                  <Input
+                    {...register('email')}
+                    className={cn({
+                      'focus-visible:ring-red-500': errors.email,
+                    })}
+                    placeholder="you@example.com"
+                  />
+                  {errors?.email && <p className="text-sm text-red-500">{errors.email.message}</p>}
                 </div>
 
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="password">Password</Label>
-                  <Input className={cn({ 'focus-visible:ring-red-500': errors.password })} placeholder="password" type="password" {...register('password')} />
+                  <Input
+                    {...register('password')}
+                    type="password"
+                    className={cn({
+                      'focus-visible:ring-red-500': errors.password,
+                    })}
+                    placeholder="Password"
+                  />
+                  {errors?.password && <p className="text-sm text-red-500">{errors.password.message}</p>}
                 </div>
+
                 <Button>Sign up</Button>
               </div>
             </form>
