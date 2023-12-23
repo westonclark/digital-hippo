35cf2f780d1a2ea27b79d96ef06835a8a99d3eee
Weston Clark <westoclark@gmail.com>
2023-12-23 08:59:30 -0800

    move payload-config

--- src/lib/validators/payload-utils.ts
+++
@@ -1,6 +0,0 @@
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
-import { NextRequest } from 'next/server';
-
-export const getServerSideUser = async (cookies: NextRequest['cookies'] | ReadonlyRequestCookies) => {
-  const token = cookies.get('payload-token');
-};
