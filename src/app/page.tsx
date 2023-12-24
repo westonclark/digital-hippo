8f939f9e1243156041d63e94aae23be9d2aa6a57
Weston Clark <westoclark@gmail.com>
2023-12-24 14:51:43 -0800

    display products on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import ProductReel from '@/components/ProductReel';
 import { Button, buttonVariants } from '@/components/ui/button';
 import { ArrowDownToLine, CheckCircle, Leaf } from 'lucide-react';
-import Image from 'next/image';
 import Link from 'next/link';
 
 const perks = [
-  { name: 'Instant Delivery', Icon: ArrowDownToLine, Description: 'Get your assets delivered to your email in seconds or downlad them right away' },
-  { name: 'Guaranteed Quality', Icon: CheckCircle, Description: 'Every Asset on our platform is verified by our team to ensure our high quality standard' },
-  { name: 'For the Planet', Icon: Leaf, Description: "we've pledged 1% of sales to the preservation and restoration of the natural enviroment" },
+  {
+    name: 'Instant Delivery',
+    Icon: ArrowDownToLine,
+    description: 'Get your assets delivered to your email in seconds and download them right away.',
+  },
+  {
+    name: 'Guaranteed Quality',
+    Icon: CheckCircle,
+    description: 'Every asset on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.',
+  },
+  {
+    name: 'For the Planet',
+    Icon: Leaf,
+    description: "We've pledged 1% of sales to the preservation and restoration of the natural environment.",
+  },
 ];
 
 export default function Home() {
@@ -16,17 +28,18 @@ export default function Home() {
       <MaxWidthWrapper>
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Your marketplace for high-quality <span className="text-primary">digital assets</span>
+            Your marketplace for high-quality <span className="text-blue-600">digital assets</span>.
           </h1>
-          <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to DigitalHippo. Every asset on our platform is verified by our team to ensure the highest quality product.</p>
-          <div className="flex felx-cal sm:flex-row gap-4 mt-6">
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to DigitalHippo. Every asset on our platform is verified by our team to ensure our highest quality standards.</p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
             <Link href="/products" className={buttonVariants()}>
               Browse Trending
             </Link>
             <Button variant="ghost">Our quality promise &rarr;</Button>
           </div>
         </div>
-        {/* TODO: List Products */}
+
+        <ProductReel query={{ sort: 'desc', limit: 4 }} href="/products?sort=recent" title="Brand new" />
       </MaxWidthWrapper>
 
       <section className="border-t border-gray-200 bg-gray-50">
@@ -37,9 +50,10 @@ export default function Home() {
                 <div className="md:flex-shrink-0 flex justify-center">
                   <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">{<perk.Icon className="w-1/3 h-1/3" />}</div>
                 </div>
+
                 <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
                   <h3 className="text-base font-medium text-gray-900">{perk.name}</h3>
-                  <p className="mt-3 text-sm text-muted-foreground">{perk.Description}</p>
+                  <p className="mt-3 text-sm text-muted-foreground">{perk.description}</p>
                 </div>
               </div>
             ))}
