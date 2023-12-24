d10e928d8b5fe2f19b8e19031fd03cee55196c97
Weston Clark <westoclark@gmail.com>
2023-12-24 13:14:31 -0800

    remove stripe from products collection

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -2,7 +2,7 @@ import { AfterChangeHook, BeforeChangeHook } from 'payload/dist/collections/conf
 import { PRODUCT_CATEGORIES } from '../../config';
 import { Access, CollectionConfig } from 'payload/types';
 import { Product, User } from '../../payload-types';
-import { stripe } from '../../lib/stripe';
+// import { stripe } from '../../lib/stripe';
 
 const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
   const user = req.user;
@@ -75,42 +75,42 @@ export const Products: CollectionConfig = {
     afterChange: [syncUser],
     beforeChange: [
       addUser,
-      async (args) => {
-        if (args.operation === 'create') {
-          const data = args.data as Product;
+      // async (args) => {
+      //   if (args.operation === 'create') {
+      //     const data = args.data as Product;
 
-          const createdProduct = await stripe.products.create({
-            name: data.name,
-            default_price_data: {
-              currency: 'USD',
-              unit_amount: Math.round(data.price * 100),
-            },
-          });
+      //     const createdProduct = await stripe.products.create({
+      //       name: data.name,
+      //       default_price_data: {
+      //         currency: 'USD',
+      //         unit_amount: Math.round(data.price * 100),
+      //       },
+      //     });
 
-          const updated: Product = {
-            ...data,
-            stripeId: createdProduct.id,
-            priceId: createdProduct.default_price as string,
-          };
+      //     const updated: Product = {
+      //       ...data,
+      //       stripeId: createdProduct.id,
+      //       priceId: createdProduct.default_price as string,
+      //     };
 
-          return updated;
-        } else if (args.operation === 'update') {
-          const data = args.data as Product;
+      //     return updated;
+      //   } else if (args.operation === 'update') {
+      //     const data = args.data as Product;
 
-          const updatedProduct = await stripe.products.update(data.stripeId!, {
-            name: data.name,
-            default_price: data.priceId!,
-          });
+      //     const updatedProduct = await stripe.products.update(data.stripeId!, {
+      //       name: data.name,
+      //       default_price: data.priceId!,
+      //     });
 
-          const updated: Product = {
-            ...data,
-            stripeId: updatedProduct.id,
-            priceId: updatedProduct.default_price as string,
-          };
+      //     const updated: Product = {
+      //       ...data,
+      //       stripeId: updatedProduct.id,
+      //       priceId: updatedProduct.default_price as string,
+      //     };
 
-          return updated;
-        }
-      },
+      //     return updated;
+      //   }
+      // },
     ],
   },
   fields: [
