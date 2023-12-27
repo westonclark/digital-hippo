5ffa3b62c2d87359df1c369b49a92657407f9596
Weston Clark <westoclark@gmail.com>
2023-12-27 10:28:09 -0800

    add basic icon packs

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -38,7 +38,7 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
         })}
         href={`/product/${product.id}`}>
         <div className="flex flex-col w-full">
-          {/* <ImageSlider urls={validUrls} /> */}
+          <ImageSlider urls={validUrls} />
 
           <h3 className="mt-4 font-medium text-sm text-gray-700">{product.name}</h3>
           <p className="mt-1 text-sm text-gray-500">{label}</p>
