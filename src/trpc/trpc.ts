a369774eb80a83914e656653a9dcdcae4193b860
Weston Clark <westoclark@gmail.com>
2023-12-27 23:06:26 -0800

    init stripe and payment router

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,21 @@
 import { ExpressContext } from '@/server';
-import { initTRPC } from '@trpc/server';
+import { TRPCError, initTRPC } from '@trpc/server';
+import { PayloadRequest } from 'payload/types';
 
 const t = initTRPC.context<ExpressContext>().create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
+const middleware = t.middleware;
+
+const isAuth = middleware(async ({ ctx, next }) => {
+  const req = ctx.req as PayloadRequest;
+  const { user } = req as PayloadRequest;
+
+  if (!user || !user.id) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+
+  return next({ ctx: { user } });
+});
+
+export const privateProcedure = t.procedure.use(isAuth);
