320cef75d73540b4443e23c6ff7b8cd8d34f2e2b
Weston Clark <westoclark@gmail.com>
2023-12-29 10:57:32 -0800

    add footer

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx';
+import { Metadata } from 'next';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -8,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: 'USD' | 'EUR' | 'GBO' | 'BDT';
+    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT';
     notation?: Intl.NumberFormatOptions['notation'];
   } = {}
 ) {
@@ -16,5 +17,53 @@ export function formatPrice(
 
   const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
 
-  return Intl.NumberFormat('en-US', { style: 'currency', currency, notation, maximumFractionDigits: 2 }).format(numericPrice);
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
+}
+
+export function constructMetadata({
+  title = 'DigitalHippo - the marketplace for digital assets',
+  description = 'DigitalHippo is an open-source marketplace for high-quality digital goods.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@joshtriedcoding',
+    },
+    icons,
+    metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
